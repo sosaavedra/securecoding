f09f4a0becacaf53ab985208d3aa5a8f476dc559
Praveer <raipraveer@gmail.com>
2014-10-23 02:40:31 +0200

    code review in register.php, improved validations. adding code to send email and generate transaction codes

--- website/register.php
+++ website/register.php
@@ -1,56 +1,60 @@
  
 <?php
-$con = mysqli_connect ( "localhost", "root", "samurai", "banksys" );
-// Check connection
-if (mysqli_connect_errno ()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error ();
-}
 
-// escape variables for security
-$title = mysqli_real_escape_string ( $con, $_POST ['title'] );
-$firstname = mysqli_real_escape_string ( $con, $_POST ['firstname'] );
-$lastname = mysqli_real_escape_string ( $con, $_POST ['lastname'] );
-$email = mysqli_real_escape_string ( $con, $_POST ['email'] );
-$password = mysqli_real_escape_string ( $con, $_POST ['password'] );
-$cpassword = mysqli_real_escape_string ( $con, $_POST ['cpassword'] );
-
-// define variables and set to empty values
-$fnameErr = $lnameErr = $emailErr = $passErr = $cpassErr = $passMatchErr = "";
-$formValid = true;
-
-if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+// checking if request is a post request(to avoid get attacks)
+if ($_POST) {
 	
-	if (empty ( $firstname )) {
-		$fnameErr = "First name is required";
+	// define variables for form values validation and set to empty values
+	$fnameErr = $lnameErr = $emailErr = $passErr = $cpassErr = $passMatchErr = "";
+	$formValid = true;
+	
+
+	
+	if (empty ( $_POST ['firstname'] ) || ! preg_match ( "/^[a-zA-Z ]*$/", $_POST ['firstname'] )) {
+		$fnameErr = "First name not proper";
 		$formValid = false;
 	}
 	
-	if (empty ( $lastname )) {
-		$lnameErr = "Last name is required";
+	if (empty ( $_POST ['lastname'] ) || ! preg_match ( "/^[a-zA-Z ]*$/", $_POST ['lastname'] )) {
+		$lnameErr = "Last name not proper";
 		$formValid = false;
 	}
 	
-	if (empty ( $email )) {
-		$emailErr = "Email is required";
+	if (empty ( $_POST ['email'] ) || ! filter_var ( $_POST ['email'], FILTER_VALIDATE_EMAIL )) {
+		$emailErr = "Email is not proper";
 		$formValid = false;
 	}
 	
-	if (empty ( $password )) {
+	if (empty ( $_POST ['password'] )) {
 		$passErr = "Password is required";
 		$formValid = false;
 	}
 	
-	if (empty ( $cpassword )) {
+	if (empty ( $_POST ['cpassword'] )) {
 		$cpassErr = "Retype password is required";
 		$formValid = false;
 	}
 	
-	if (strcmp ( $password, $cpassword ) != 0) {
+	if (strcmp ( $_POST ['password'], $_POST ['cpassword'] ) != 0) {
 		$passMatchErr = "Passwords do not match";
 		$formValid = false;
 	}
 	
 	if ($formValid) {
+		// connect to DB
+		$con = mysqli_connect ( "localhost", "root", "samurai", "banksys" );
+		// Check connection
+		if (mysqli_connect_errno ()) {
+			echo "Failed to connect to MySQL: " . mysqli_connect_error ();
+		}
+		
+		// escape variables for security
+		$title = mysqli_real_escape_string ( $con, $_POST ['title'] );
+		$firstname = mysqli_real_escape_string ( $con, $_POST ['firstname'] );
+		$lastname = mysqli_real_escape_string ( $con, $_POST ['lastname'] );
+		$email = mysqli_real_escape_string ( $con, $_POST ['email'] );
+		$password = mysqli_real_escape_string ( $con, $_POST ['password'] );
+		$cpassword = mysqli_real_escape_string ( $con, $_POST ['cpassword'] );
 		
 		// insert into client table
 		$sql = "INSERT INTO `client` (`first_name`, `last_name`, `email`) VALUES
@@ -76,8 +80,8 @@ if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 	else {
 		header ( 'Location: registration.html' );
 	}
-}	else {
-		header ( 'Location: error.html' );
-	}
+} else {
+	header ( 'Location: error.html' );
+}
 
 ?>
---
+++ website/sendEmail.php
@@ -0,0 +1,65 @@
+<?php
+//sendNewCustEMail ( 1 );
+function sendNewCustEMail($customerId) {
+	
+	// code for connecting to DB and fetching transaction codes for a customer
+	$con = mysqli_connect ( "localhost", "root", "samurai", "banksys" );
+	// Check connection
+	if (mysqli_connect_errno ()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error ();
+	}
+	
+	$checkSql = "SELECT * FROM tan_code WHERE client_id = '{$customerId}'";
+	$result = mysqli_query ( $con, $checkSql );
+	
+	if (! $result) {
+		die ( 'Error: ' . mysqli_error ( $con ) );
+	}
+	
+	// make email body with codes
+	$counter = 1;
+	$mailBody = "";
+	while ( $row = mysqli_fetch_array ( $result ) ) {
+		$mailBody .= "<tr><td>" . $counter . "</td><td>" . $row ['code'] . "</td></tr>";
+		$counter ++;
+	}
+	
+	mysqli_close ( $con );
+	
+	// code to send email
+	
+	require_once '/home/samurai/Praveer/misc/vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+	
+	$transport = Swift_SmtpTransport::newInstance ( 'smtp.gmail.com', 465, "ssl" )->setUsername ( 'scteam17@gmail.com' )->setPassword ( 'samurai17' );
+	
+	$mailer = Swift_Mailer::newInstance ( $transport );
+	
+	$emailBody = '
+<html>
+<head>
+  <title>Your transaction codes</title>
+</head>
+<body>
+  <p>Thank you for registering! Here are your secure transaction codes, Please keep them secret</p>
+  <table border="1">
+    <tr>
+      <th>Serial number</th><th>Transaction code</th>
+    </tr>
+    <tr>' . $mailBody . '</tr>
+  </table>
+</body>
+</html>
+';
+	
+	$message = Swift_Message::newInstance ( 'IMP:CONFIDENTIAL: your transation codes' )
+	
+	->setFrom ( array ('scteam17@gmail.com' => 'Secure Banking' ) )
+	->setTo ( array ('raipraveer@gmail.com'	) )
+	->setBody ( $emailBody, 'text/html' );
+	
+	// echo $emailBody;
+	
+	$result = $mailer->send ( $message );
+}
+
+?>
