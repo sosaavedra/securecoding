04305e032007f1fb53b1b52d7932ed5f839f84e4
sosaavedra <sosaavedra@gmail.com>
2014-12-02 06:23:03 +0100

    The latests

--- website/transfer.php
+++ website/transfer.php
@@ -115,12 +115,7 @@ if ($_POST) {
             // Check if $uploadOk is set to 0 by an error
             if ($fileValid) {
                 if (move_uploaded_file ( $_FILES ["uploadFile"] ["tmp_name"], $target_dir )) {
-                    $output = "";
-                    ob_start();
-                    $uploadErr = system("./c-parser/parser $target_dir $client_id", $output);
-                    $content = ob_get_contents();
-
-                    $uploadErr = $content;
+                    $uploadErr = shell_exec("./c-parser/parser $target_dir $client_id 2>&1");
 
                     if($uploadErr == ""){
                         header ( 'Location: transferSuccess.html' );
