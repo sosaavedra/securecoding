9113601bcec26bfe3e74edf72aadc7664e44af53
sosaavedra <sosaavedra@gmail.com>
2015-01-24 20:46:11 +0100

    Remove unused files

--- tools for whitebox/phpteam18/views/footer.php
+++
@@ -1,7 +0,0 @@
-		<footer>
-			<h4 style="text-align: center">&copy; TUM 2014</h4>
-		</footer>
-      	<hr>
-    </div>
-  </body>
-</html>
--- tools for whitebox/rats-2.4/unistd.h
+++
@@ -1,5 +0,0 @@
-/* This is a dummy include file that is required by the source files generated
- * by flex.
- */
-#include <io.h>
-#include <stdlib.h>
